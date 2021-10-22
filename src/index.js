e9d6e6c220f23c6571fef68d096ed16e1afa46e5
yakov cohen <yakovc1431@gmail.com>
2021-10-22 12:19:13 +0300

    middileware (errorHandler ,userHandler)
    add errorHandler
    and userHandler

--- src/index.js
+++ src/index.js
@@ -10,23 +10,34 @@ app.use(express.json())     // parses requests as json
 const pokemonRouter = require('./routers/pokemonRouter');
 const userRouter = require('./routers/userRouter');
 
+const {errorHandler} = require('./middleware/errorHandler')
+const {userHandler} = require('./middleware/userHandler')
+
+// middleware userHandler
+app.use(userHandler)
+
 // http//localhost:8080/pokemon
 app.use('/pokemon', pokemonRouter);
+
+// http//localhost:8080/info
 app.use('/info', userRouter);
 
-app.get("/" , (req, res) => {
-  res.send("hello world")
-  P.getPokemonByName('1') 
-  .then(function(response) {
-    console.log(response.name);
-    console.log(response.height);
-    console.log(response.weight);
-    console.log(response.types[0].type.name);
-  })
-  .catch(function(error) {
-    console.log('There was an ERROR: ', error);
-  });
-})
+// middleware errorHandler
+app.use(errorHandler);
+
+// app.get("/" , (req, res) => {
+//   res.send("hello world")
+//   P.getPokemonByName('1') 
+//   .then(function(response) {
+//     console.log(response.name);
+//     console.log(response.height);
+//     console.log(response.weight);
+//     console.log(response.types[0].type.name);
+//   })
+//   .catch(function(error) {
+//     console.log('There was an ERROR: ', error);
+//   });
+// })
 
 // start the server
 app.listen(port, function() {
--- src/routers/pokemonRouter.js
+++ src/routers/pokemonRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Pokedex = require('pokedex-promise-v2');
 const P = new Pokedex();
+
 const fs = require('fs')
 const path = require('path')
 
@@ -9,16 +10,14 @@ const path = require('path')
 // http//localhost:8080/pokemon/get/:id
 router.get('/get/:id',(req, res) => {
     const id = req.params.id;
-    console.log("you in /get/:id")
     P.getPokemonByName(id)
     .then(function(response) {
             const pokedexObj2 = createPokeObj(response)
-            console.log(pokedexObj2);
-            res.send(pokedexObj2);
+            return res.send(pokedexObj2);
         })
     .catch(function(error) {
             console.log('There was an ERROR: ', error);
-            res.send('no');
+            // throw {"status": 403, "messege": "no such Pokemon, try again"};
         });
 })
 
@@ -27,18 +26,17 @@ router.put("/catch/:id", (req, res)=> {
     const id = req.params.id;
     const userName = req.headers.username;
     const pokemon = JSON.stringify(req.body.pokemon);
-    const uesrFolderPath = path.resolve(`.\\users`, userName);
+    const userFolderPath = path.resolve(`.\\users`, userName);
 
-    if (fs.existsSync(uesrFolderPath)){                             // if uesr name exists uesrs
-        const collection = fs.readdirSync(uesrFolderPath)
+    if (fs.existsSync(userFolderPath)){                             // if uesr name exists uesrs
+        const collection = fs.readdirSync(userFolderPath)
         if (collection.includes(`${id}.json`)){                     // if in user name exists the pokemon
-            res.status(403).json({ message: 'An error occured'})    // change status to 403
+            throw { "status": 403,                                  // change status to 403
+                    "messege": "Pokemon already caught"}; 
         }
-    }else{
-        fs.mkdirSync(uesrFolderPath)                                // create a folder with the username 
     }
-    fs.writeFileSync(`${uesrFolderPath}/${id}.json`, `${pokemon}`); // create pokemon to user name
-    res.send(true)
+    fs.writeFileSync(`${userFolderPath}/${id}.json`, `${pokemon}`); // create pokemon to user name
+    return res.send(true)
 })
 
 
@@ -46,36 +44,31 @@ router.put("/catch/:id", (req, res)=> {
 router.delete('/release/:id',(req, res) => {
     const id = req.params.id;
     const userName = req.headers.username;
-    const uesrFolderPath = path.resolve(`.\\users`, userName);
+    const userFolderPath = path.resolve(`.\\users`, userName);
 
-    if (fs.existsSync(uesrFolderPath)){                                 // if uesr name exists uesrs
-        const collection = fs.readdirSync(uesrFolderPath);              // array with files pokemon
-        if (!collection.includes(`${id}.json`)){                        // if not in collection
-            res.status(403).json(
-                {message: 'the pokemon is not on your collection'})     // change status to 403
-        }
-        fs.unlinkSync(`${uesrFolderPath}/${id}.json`);                  // delete file
-        res.send(true);
+    const collection = fs.readdirSync(userFolderPath);              // array with files pokemon
+    if (!collection.includes(`${id}.json`)){                        // if not in collection
+        throw { "status": 403,                                      // change status to 403
+                "messege": "Pokemon is no in your collection"}; 
     }
-    res.status(403).json({ message: 'no such user'});                   // change status to 403
+    fs.unlinkSync(`${userFolderPath}/${id}.json`);                  // delete file
+    return res.send(true);
+    
 })
 
 // http://localhost:8080/pokemon/
 router.get('/',(req, res) => {
     const userName = req.headers.username;
-    const uesrFolderPath = path.resolve(`.\\users`, userName);
+    const userFolderPath = path.resolve(`.\\users`, userName);
     const pokemonCollection = [];
-
-    if (!fs.existsSync(uesrFolderPath)){                                 // if not uesr name exists uesrs
-        res.status(403).json({ message: 'no such user'});                // change status to 403
-    }
-    const collection = fs.readdirSync(uesrFolderPath);                   // array with files pokemon
+    const collection = fs.readdirSync(userFolderPath);                   // array with files pokemon
+    
     collection.forEach(pokemonFile => {
-        const filePath = path.resolve(uesrFolderPath, pokemonFile);      // path of file 
+        const filePath = path.resolve(userFolderPath, pokemonFile);      // path of file 
         const pokemonObj = fs.readFileSync(filePath);                    // pokemon file object
         pokemonCollection.push(pokemonObj.toString())
     });
-    res.send(pokemonCollection);                                         // send array of object pokemon
+    return res.send(pokemonCollection);                                  // send array of pokemons object 
 })
 
 //create Poke Object from response
